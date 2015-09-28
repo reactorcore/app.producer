4f1df6b7f7faecacbaa9ed9a1168e637dcc870d2
azellsworth <azellsworth@gmail.com>
2015-09-28 15:56:44 +0200

    Add basic dropdown menu for events

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -3,7 +3,7 @@ angular.module('producer.main', [])
 .controller('mainController', function ($scope, Template) {
   $scope.template = {role: '', event: '', description: ''};
   $scope.submitTemplate = function() {
-    $scope.buttonClicked = true;
+    console.log("Template: ", $scope.template)
     Template.submitTemplate($scope.template);
   }
 });
