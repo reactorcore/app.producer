f76415b73c1d4c8a87f21a49cc05c77171b872aa
azellsworth <azellsworth@gmail.com>
2015-10-06 15:23:22 -0700

    Code cleanup, remove unnecessary dependences

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,4 +1,3 @@
-var jwt  = require('jwt-simple');
 
 module.exports = {
   errorLogger: function (error, req, res, next) {
@@ -7,7 +6,7 @@ module.exports = {
     console.error(error.stack);
     next(error);
   },
-  
+
   errorHandler: function (error, req, res, next) {
     // send error message to client
     // message for gracefull error handling on app
