8eb72b084300ff77056672a85e7dd49487ff330e
SARA GUDEMAN <sgudeman982@gmail.com>
2015-10-07 19:36:24 -0700

    (refactor)removes api key, addTask accepts arguments

--- server/asana/asana.js
+++ server/asana/asana.js
@@ -1,19 +1,8 @@
 var Asana = require('asana');
-// May need to require utils later. Haven't looked into it yet.
 
-// TODO: using API key for basic auth. Will change later.
-// TODO: add api key
-
-// REFERENCE:
-// dispatcher: basic wrapper for making HTTP requests to API + handles auth
-// Does get/post requests + handles API url, etc.
-
-var client = Asana.Client.create().useBasicAuth("eci1iJsm.WkjGC3onSSsYE30OaJ2zUme");
-// it seems like we could just create a posting/create tasks function w/out the client.users.me()?
-// .me returns current user of the dispatcher
-// function needs to take params: assignee, name, workspace or project (according to docs)
+var client = Asana.Client.create().useBasicAuth("API_KEY_HERE");
 module.exports = {
-  addTask: function() {
+  addTask: function(task, description) {
     client.users.me()
       .then(function(user) {
         var userId = user.id;
@@ -21,9 +10,10 @@ module.exports = {
         console.log('workspace id:', workspaceId);
         return client.tasks.create({
           assignee: userId,
-          name: "Hello",
+          name: task,
           workspace: "498346170860",
-          projects: "56218871247864"
+          projects: "56218871247864",
+          notes: description
         });
       })
       .then(function(response) {
@@ -35,16 +25,3 @@ module.exports = {
       });
     }
 };
-
-// Every task is required to be created in a specific workspace, and this workspace cannot be changed once set. The workspace need not be set explicitly if you specify a project or a parent task instead.
-
-// interesting for use later
-// SELECTING FIELDS
-// "data": {                            this
-//   "id": 1001,                        
-//   "name": "Feed the cat",            this.name        
-//   "workspace": {                     this.workspace
-//     "id": 14916,
-//     "name": "Shared Projects",
-//   },
-//   ....
