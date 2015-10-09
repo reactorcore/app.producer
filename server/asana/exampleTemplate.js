60ba876a4cb8c9091668873cc8fa5dc12910f90d
SARA GUDEMAN <sgudeman982@gmail.com>
2015-10-08 17:20:32 -0700

    (feat) posts template info to asana

---
+++ server/asana/exampleTemplate.js
@@ -0,0 +1,16 @@
+{ 
+  "id": 47159080788845,
+  "name": "Template Task 1",
+  "assignee": null,
+  "notes": "Who: https://app.asana.com/0/47159080788837/47159080788839\nWho: https://app.asana.com/0/47159080788837/47742278742250 \nWhen: MTWRFS\n\nDescription for task 1\nFollow-Up: https://app.asana.com/0/47159080788843/47159080788849\n\nTemplate: https://app.asana.com/0/47159080788843/47159080788845",
+  "projects": [ { "id": 47159080788843, "name": "$$TEMPLATE: Testing" } ],
+  "tags": [],
+  "memberships": [ { "project": { "id": 47159080788843, "name": "$$TEMPLATE: Testing" }, "section": null } ],
+  "workspace": { "id": 498346170860, "name": "Personal Projects" },
+  "subtasks": [ { "id": 47742278742244, "name": "dothis" }, { "id": 47742278742246, "name": "then that" } ],
+  "assignments": [ "47159080788839", "47742278742250"],
+  "events": [ "M", "T", "W", "R", "F", "S" ],
+  "link": "https://app.asana.com/0/47159080788843/47159080788845",
+  "timezone": "America/Los_Angeles",
+  "follow_ups": [ "https://app.asana.com/0/47159080788843/47159080788849" ]
+}
