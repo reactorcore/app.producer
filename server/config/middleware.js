6db33ed529e087f7d5e790044c416e8b26b9a937
SARA GUDEMAN <sgudeman982@gmail.com>
2015-10-20 20:25:54 -0700

    (refactor)moves router into routes.js

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,21 +3,15 @@ var morgan      = require('morgan'), // used for logging incoming request
     helpers     = require('./helpers.js'); // our custom middleware
 
 
-module.exports = function (app, express) {
+module.exports = function (app) {
   // The following sets up the routers for user authentication and the templates API
-  var router = express.Router();
 
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../client'));
-
-  app.use(router); // use templates router for all templates requests
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
-  // inject router into our routes.js file
-  require('../routes.js')(router);
-
+  require('../routes.js')(app);
 };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
-require('dotenv').load();
-var app = express();
+var bootable = require('app-boot');
+var app = require('express')();
+
+require('./config/config.js');
 
 // configure our server with all the middleware and and routing
-require('./config/middleware.js')(app, express);
+require('./config/middleware.js')(app);
 
 // export our app for testing and flexibility, required by index.js
 
