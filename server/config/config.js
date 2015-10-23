4457cfbcd66995607aab8841949897edc08c0b8f
azellsworth <azellsworth@gmail.com>
2015-10-22 19:15:10 -0700

    Beginning attempts to short circuit dotenv if .env file does not exist

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,24 @@
+var fs = require('fs');
+
 module.exports = function() {
-  require('dotenv').load();
+  console.log('looking for file')
+  console.log('current directory: ', __dirname)
+  fs.statSync(__dirname + '/../../.env', function(err, stat){
+    if(!err) {
+      console.log('File exists + I am currently in');
+      require('dotenv').load();
+    } else {
+      console.log('File doesnt exist!');
+    }
+  });
+  // try {
+  //   var env = fs.lstatSync('/../../.env');
+  //   if (env.isFile()){
+  //     console.log('ENV FOUND');
+  //     require('dotenv').load();
+  //   }
+  // }
+  // catch (error) {
+  //   console.log(error);
+  // }
 };
