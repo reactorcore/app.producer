81092c63e8bb0f5964a1cb3964c7774253a1e8c4
azellsworth <azellsworth@gmail.com>
2015-10-26 10:44:11 -0700

    Dotenv successfully short circuits when no .env file is present

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,18 @@
 var fs = require('fs');
 
 module.exports = function() {
-  // console.log('looking for file')
-  // console.log('current directory: ', __dirname)
-  // fs.stat(__dirname + '/../../.env', function(err, stat){
-  //   if(!err) {
-  //     console.log('File exists!');
-  //     require('dotenv').load();
-  //   } else {
-  //     console.log('File doesnt exist!');
-  //   }
-  // });
-  // try {
-  //   var env = fs.lstatSync(__dirname + '/../../.env');
-  //   if (env.isFile()){
-  //     console.log('ENV FOUND');
-  //     require('dotenv').load();
-  //   }
-  // }
-  // catch (error) {
-  //   console.log(error);
-  // }
-  require('dotenv').load();
+  // This function synchronously checks to see if .env is available. 
+  // If available, it loads the dotenv npm module, giving global access
+  // to process.env variables. If not available, it short circuits the 
+  // dotenv module.
+  try {
+    var env = fs.lstatSync(__dirname + '/../../.env');
+    if (env.isFile()){
+      console.log('ENV FOUND');
+      require('dotenv').load();
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
 };
