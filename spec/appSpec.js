9cc002f8cb62a667985d544152b4859530cf0bc6
SARA GUDEMAN <sgudeman982@gmail.com>
2015-10-30 11:32:05 -0700

    (refactor)removes jasmine examples + adds initial test file

---
+++ spec/appSpec.js
@@ -0,0 +1,8 @@
+describe("Events service requests", function() {
+  // testing GET request to /events
+  // must return a JSON object
+  // JSON object must contain a 'text' key
+  it("responds with a JSON object upon GET request", function() {
+    expect(true).toBe(true);
+  });
+});
