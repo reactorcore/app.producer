ebf1bab03c15e7111e99d4e02bc534d17fb732fa
SARA GUDEMAN <sgudeman982@gmail.com>
2015-10-31 13:40:56 -0700

    (feat)adds basic producer.main test + mocks services w/empty fcns

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,8 +1,36 @@
-describe("Events service requests", function() {
-  // testing GET request to /events
-  // must return a JSON object
-  // JSON object must contain a 'text' key
-  it("responds with a JSON object upon GET request", function() {
-    expect(true).toBe(true);
+describe("producer.main module", function() {
+  // set up module
+  beforeEach(module("producer.main"));
+
+  var $controller, TemplateServiceMock, EventsServiceMock, RolesServiceMock;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  beforeEach(inject(function(){
+    TemplateServiceMock = {
+      submitTemplate: function() {},
+    };
+    EventsServiceMock = {
+      getEvents: function() {},
+      submitEvent: function() {}
+    };
+    RolesServiceMock = {
+      getRoles: function() {}
+    };
+  }));
+
+  describe("mainController controller", function() {
+    it("should be defined", function() {
+      var $scope = {};
+      var mainController = $controller('mainController', { 
+        $scope: $scope, 
+        Template:  TemplateServiceMock, 
+        Roles: RolesServiceMock, 
+        Events: EventsServiceMock});
+      expect(mainController).toBeDefined();
+    });    
   });
 });
