4e34cf4f68e5c1a560e7e762792869d8dae43b5b
SARA GUDEMAN <sgudeman982@gmail.com>
2015-11-17 15:21:33 -0800

    (refactor)implements new db CRUD operations

--- server/procedures/proceduresController.js
+++ server/procedures/proceduresController.js
@@ -1,43 +1,62 @@
-var Procedure = require('./../database/models/procedure.js').Procedure;
+var Procedure = require('./../database/models/procedure.js');
+var db = require('./../database/db.js');
 
 module.exports = {
-  getProcedures: function(req, res, next) {
-    var procedureDBModel = new Procedure();
-
-    procedureDBModel.getAllAsync()
-    .then(function(procedures) {
-      res.status(200);
-      res.send(procedures);
-    })
-    .catch(function(err) {
-      console.error(new Error(err).stack);
-      res.sendStatus(500);
-    });
+  getProcedures: function(req, res) {
+    // TODO: GET ALL PROCEDURES
   },
 
-  createProcedure: function(req, res, next) {
+  createProcedure: function (req, res) {
     var procedureData = req.body;
     var procedureDBModel = new Procedure(procedureData);
 
-    procedureDBModel.save()
-    .then(function(newID) {
-      procedureData.ID = newID;
-      res.status(201);
-      res.send(procedureData);
-    })
-    .catch(function(err) {
-      console.error(new Error(err).stack);
-      res.sendStatus(500);
+    db.create('procedure', procedureDBModel, function (err, data) {
+      if (err) {
+        console.log('Error in createProcedure ', err);
+      } else {
+        res.status(201);
+        res.send(data);
+      }
     });
   },
 
-  getProcedureName: function(req, res, next, name){
-    req.procedureName = name;
-    next()
+  createProcedure: function (req, res) {
+    var procedureId = req.body.id;
+
+    db.update('procedure', procedureId, function (err, data) {
+      if (err) {
+        console.log('Error in updateProcedure ', err);
+      } else {
+        res.status(201);
+        res.send(data);
+      }
+    });
   },
 
-  deleteProcedure: function(req, res, next) { 
-    var procedureName = req.procedureName;
+  getProcedure: function (req, res){
+    var procedureId = req.body.id;
+
+    db.read('procedure', procedureId, function (err, data) {
+      if (err) {
+        console.log('Error in getProcedure ', err);
+      } else {
+        res.status(201);
+        res.send(data);        
+      }
+    });
+  },
+
+  deleteProcedure: function (req, res) { 
+    var procedureId = req.body.id;
+
+    db.destroy('procedure', procedureId, function (err, data) {
+      if (err) {
+        console.log('Error in deleteProcedure ', err);
+      } else {
+        res.status(201);
+        res.send(data);
+      }
+    });
 
   }
 };
