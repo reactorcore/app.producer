78860f273796f448ae9039765509301e35229042
SARA GUDEMAN <sgudeman982@gmail.com>
2015-11-18 14:21:00 -0800

    (refactor)cleans up dbConfig, fixes error in cb for procedureMethods

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -1,11 +1,10 @@
 var path = require('path');
-
 var env = process.env.NODE_ENV || 'development';
-var dbPath = './data/' + env;
 
-console.log("Using database: " + path.join(__dirname, dbPath));
+var dbPath = path.join(__dirname, './data/', env);
+console.log("Using database: ", dbPath);
 
-var database = path.join(__dirname, dbPath);
-
-module.exports = exports = database;
+module.exports = exports = {
+  database: dbPath
+};
 
--- server/procedures/proceduresController.js
+++ server/procedures/proceduresController.js
@@ -1,6 +1,8 @@
 var Procedure = require('./../database/models/procedure.js');
 var db = require('./../database');
 
+// NOTE: leveldb put + delete callbacks do not accept a second param for data
+
 module.exports = {
   getProcedures: function(req, res) {
     // TODO: GET ALL PROCEDURES
@@ -10,6 +12,7 @@ module.exports = {
     var procedureData = req.body;
     var procedureDBModel = new Procedure(procedureData);
 
+    // TODO: remove data param in callback--second arg does not exist in leveldb put method
     db.create('procedure', procedureDBModel, function (err, data) {
       if (err) {
         console.log('Error in createProcedure ', err);
@@ -21,11 +24,13 @@ module.exports = {
   },
 
   updateProcedure: function (req, res) {
+    // TODO: remove id from model instance
     var procedureId = req.body.id;
     var procedureData = req.body;
 
     var procedureDBModel = new Procedure(procedureData);
 
+    // TODO: remove data param in callback--second arg does not exist in leveldb put method
     db.update('procedure', procedureId, procedureDBModel, function (err, data) {
       if (err) {
         console.log('Error in updateProcedure ', err);
@@ -37,6 +42,7 @@ module.exports = {
   },
 
   getProcedure: function (req, res){
+    // TODO: remove id from model instance
     var procedureId = req.body.id;
 
     db.read('procedure', procedureId, function (err, data) {
@@ -52,12 +58,12 @@ module.exports = {
   deleteProcedure: function (req, res) { 
     var procedureId = req.body.id;
 
-    db.destroy('procedure', procedureId, function (err, data) {
+    // TODO: remove data param in callback--second arg does not exist in leveldb put method
+    db.destroy('procedure', procedureId, function (err) {
       if (err) {
         console.log('Error in deleteProcedure ', err);
       } else {
         res.status(201);
-        res.send(data);
       }
     });
 
