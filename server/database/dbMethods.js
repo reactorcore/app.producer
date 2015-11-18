4b2576f17b4018ef18f04d70351ac90e07af2129
SARA GUDEMAN <sgudeman982@gmail.com>
2015-11-17 22:07:52 -0800

    (refactor)adds dbMethods file to replace/rename dbClient

---
+++ server/database/dbMethods.js
@@ -0,0 +1,33 @@
+var crypto = require('crypto');
+
+var createId = function() {
+  var currentDate = (new Date()).valueOf().toString();
+  var random = Math.random().toString();
+  return crypto.createHash('sha1').update(currentDate + random).digest('hex');
+};
+
+exports = module.exports = function dbMethods (dbInstance) {
+  var dbMethods = {
+    _options: {
+      'keyEncoding': 'json',
+      'valueEncoding': 'json'
+    },
+    destroy: function (type, id, cb) {
+      dbInstance.del(type+'.'+id, dbMethods._options, cb);
+    },
+    read: function (type, id, cb) {
+      dbInstance.get(type+'.'+id, dbMethods._options, cb);
+    },
+    update: function (type, id, data, cb) {
+      dbInstance.put(type+'.'+id, data, dbMethods._options, cb);
+    },
+    create: function (type, data, cb) {
+      var id = createId();
+      data.id = id;
+      dbInstance.put(type+'.'+id, data, dbMethods._options, cb);
+    }
+
+  }
+
+  return dbMethods;
+}
