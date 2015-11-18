57c4f56cafeb8067b9e56b3047bc4ca7d2ff31d0
Matthew Brooks <mdeanbrooks@gmail.com>
2015-11-18 11:38:07 -0800

    (refactor) Adds readAll for dbClient

--- server/database/dbClient.js
+++ server/database/dbClient.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto');
+var _ = require('underscore');
 
 var createId = function() {
   var currentDate = (new Date()).valueOf().toString();
@@ -26,9 +27,41 @@ exports = module.exports = function dbMethods (dbInstance) {
       // TODO: remove adding id
       data.id = id;
       dbInstance.put(type+'.'+id, data, dbMethods._options, cb);
+    },
+
+    readAll: function (type, cb) {
+
+      result = [];
+
+      dbInstance.createReadStream()
+      .on('data', function(data) {
+        // Not all data has a JSON key. Some values might be primitive.
+        // But if they are JSON, we need to parse them.
+        // TODO: Do we need to parse it? -mdb
+        if (isJson(data.value)) {
+          data.value = JSON.parse(data.value);
+        }
+        result.push(data);
+      })
+      .on('error', function(err) {
+        console.log("Error in getAllAsync readstream: ", err);
+      })
+      .on('end', function() {
+        result = _.pluck(result, 'value');
+        cb(null, result);
+      });
     }
 
   }
 
   return dbMethods;
 }
+
+var isJson = function (str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false
+  }
+  return true;
+};
