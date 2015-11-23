299d43e99076b253139cc0730a68b86a8004f2fc
Matthew Brooks <mdeanbrooks@gmail.com>
2015-11-23 15:01:44 -0800

    (feat) rename module and add getEvents function

--- client/app/soundboard/soundboard.js
+++ client/app/soundboard/soundboard.js
@@ -1,6 +1,13 @@
-angular.module('soundboard', [])
+angular.module('producer.soundboard', [])
 
 .controller('soundboardController', function ($scope, Events) {
+  $scope.events = [];
 
+  $scope.getEvents = function () {
+    Events.getEvents().then(function (resp) {
+      var events = resp.data;
+      $scope.events = events;
+    });
+  };
 
 });
