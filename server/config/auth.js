fdd2b922223b5dd2f9fc2d7db6806389390a56dd
Matthew Brooks <mdeanbrooks@gmail.com>
2015-11-27 16:13:53 -0800

    (feat) adds initial auth middleware module with dependencies

---
+++ server/config/auth.js
@@ -0,0 +1,10 @@
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var GitHubStrategy = require('passport-github').Strategy;
+var passport = require('passport');
+var util = require('util');
+var cookieParser = require('cookie-parser');
+
+exports = module.exports = function (app) {
+
+};
