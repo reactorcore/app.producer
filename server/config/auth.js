4c0a524cffc482142e40f5af1ff1792666890166
Matthew Brooks <mdeanbrooks@gmail.com>
2015-11-27 17:22:59 -0800

    (feat) sets up auth middleware for app

--- server/config/auth.js
+++ server/config/auth.js
@@ -5,6 +5,63 @@ var passport = require('passport');
 var util = require('util');
 var cookieParser = require('cookie-parser');
 
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete GitHub profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    // what should the callback URL be here?
+    callbackURL: 'http://api.staging.hackreactor.com/auth/github/callback',
+    scope: ['read:org', 'user']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    require('request').get({
+      json: true,
+      url: 'https://api.github.com/user/teams',
+      headers: {
+                'Authorization': 'token ' + accessToken,
+                //this is definitely wrong
+                'User-Agent': 'Fulcrum'
+              }
+      },
+      function(err, res, body){
+        var authorized = false;
+        body.forEach(function(ea) {
+          if (ea.slug === TEAM_NAME && ea.organization.login === ORG_NAME) {
+            authorized = true;
+          }
+        });
+        profile.__authorized = authorized;
+        if (!authorized) {
+          profile.__error = 'No permission to access this content.';
+        }
+        done(null, profile);
+      });
+  }
+));
+
 exports = module.exports = function (app) {
+
+
+
+  // Set up initial passport
+  app.use(cookieParser());
+  app.use(session({ secret: 'nsa nsa nsa usa usa' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 };
