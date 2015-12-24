e6b38d8e8d8c655201e2a29de4effdd4ea2a2c6a
Matthew Brooks <mdeanbrooks@gmail.com>
2015-12-23 18:28:13 -0800

    (feat) makes nav bar highlights current location

--- client/app/directives/navBar.js
+++ client/app/directives/navBar.js
@@ -4,6 +4,11 @@ angular.module('navBar', [])
       restrict: 'E',
       replace: true,
       scope: true,
-      templateUrl: '/app/directives/nav-bar.html'
+      templateUrl: '/app/directives/nav-bar.html',
+      controller: function ($scope, $location) {
+        $scope.getClass = function (currentPath) {
+          return currentPath === $location.path().split('/')[1] ? 'active-nav' : '';
+        };
+      }
     };
 });
