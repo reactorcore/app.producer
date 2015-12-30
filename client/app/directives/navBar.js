517043e80d3565a4187a82acbab2f9eecf740abf
Matthew Brooks <mdeanbrooks@gmail.com>
2015-12-29 22:42:42 -0800

    (fix) attempts to resolve navbar templateUrl error in deployment

--- client/app/directives/navBar.js
+++ client/app/directives/navBar.js
@@ -4,7 +4,7 @@ angular.module('navBar', [])
       restrict: 'E',
       replace: true,
       scope: true,
-      templateUrl: '/app/directives/nav-bar.html',
+      templateUrl: 'app/directives/nav-bar.html',
       controller: function ($scope, $location) {
         $scope.getClass = function (currentPath) {
           return currentPath === $location.path().split('/')[1] ? 'active-nav' : '';
