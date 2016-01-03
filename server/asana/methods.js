9cc01a40eb710377c667575c677605d9fe87537a
azellsworth <azellsworth@gmail.com>
2016-01-02 20:22:54 -0800

    (test) hardcoded test to attempt to get roles from asana on staging server

--- server/asana/methods.js
+++ server/asana/methods.js
@@ -23,12 +23,18 @@ module.exports = function asana(client) {
       },
 
     getRoles: function(callback){
+      var USER;
       client.users.me()
         .then(function(user) {
+          USER = user;
           var userId = user.id;
           var workspaceId = user.workspaces[1].id;
-          return client.tasks.findByProject(process.env.ROLES_ID, {
-            workspace: workspaceId
+          // return client.tasks.findByProject(process.env.ROLES_ID, {
+          //   workspace: workspaceId
+          // });
+          // TEMP SOLUTION TO TEST GETTING ROLES FROM ASANA 
+          return client.tasks.findByProject(52273497420187, {
+            workspace: process.env.WORKSPACE_ID
           });
         })
         .then(function(response){
@@ -36,6 +42,7 @@ module.exports = function asana(client) {
         })
         .catch(function(error){
           console.log("Error in getting roles: ", error);
+          callback([error, USER, process.env.WORKSPACE_ID]);
         });
       }
   };
