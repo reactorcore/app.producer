8e966fcda1b521508ee981ea274e708594e6a690
naomiajacobs <naomiajacobs@gmail.com>
2016-01-03 14:31:25 -0800

    (feat) Autofill procedures and update if procedure changes

--- client/app/templates/templates.js
+++ client/app/templates/templates.js
@@ -1,12 +1,45 @@
 angular.module('producer.templates', ['ui.select','ngSanitize','alertMessage'])
 
-.controller('templatesController', function ($scope, Template, Roles, Events, Messages) {
+.controller('templatesController', function ($scope, Template, Roles, Events, Procedures, Messages) {
   //TODO: hacky way to not carry over message
   Messages.clearMessage();
   $scope.template = {title: '', role: null, event: '', description: ''};
-  $scope.tags = [];
   $scope.roles = [];
   $scope.events = [];
+  $scope.selectedEvents = [];
+  $scope.procedures = [];
+  $scope.selectedProcedure = [{text: '', originalText:''}];
+  $scope.updatingProcedure = false;
+
+  // Reset selectedProcedure if user deletes selection to prevent null errors
+  $scope.$watch(function() {
+    return $scope.selectedProcedure;
+  }, function() {
+    if ($scope.selectedProcedure === null || $scope.selectedProcedure === undefined) {
+      $scope.selectedProcedure = [{text: '', originalText:''}];
+    }
+  }, true);
+
+  // Watch procedure text for changes
+  $scope.$watch(function() {
+    if ($scope.selectedProcedure[0]) {
+      return $scope.selectedProcedure[0].text;
+    }
+  }, function(oldText, newText) {
+    // prevents triggering on first $digets loop and rendering message on page initialization
+    if (oldText !== newText) {
+      if ($scope.selectedProcedure[0]) {
+        if ($scope.selectedProcedure[0].text !== $scope.selectedProcedure[0].originalText) {
+          // trigger notification that they'll be updating the procedure
+          Messages.setMessage('Procedure text has changed - you will be updating this procedure.', 'success');
+          $scope.updatingProcedure = true;
+        } else {
+          Messages.clearMessage();
+          $scope.updatingProcedure = false;
+        }
+      }
+    }
+  });
 
   // Loads roles
   var loadRoles = function() {
@@ -19,37 +52,63 @@ angular.module('producer.templates', ['ui.select','ngSanitize','alertMessage'])
   };
   loadRoles();
 
-  // Loads events/tags
-  var loadTags = function() {
+  // Loads events
+  var loadEvents = function() {
     return Events.getEvents()
       .then(function(eventsObj) {
         $scope.events = eventsObj.data;
       });
   };
-  loadTags();
+  loadEvents();
+
+  // Loads procedures
+  var loadProcedures = function() {
+    return Procedures.getProcedures()
+      .then(function(proceduresObj) {
+        $scope.procedures = proceduresObj.data.map(function(p) {
+          p.originalTitle = p.title;
+          p.originalText = p.text;
+          return p;
+        });
+      });
+  };
+  loadProcedures();
 
   // Change style border
-  $scope.checkInput = function(name) {
-    var el = document.getElementById('template__' + name);
-    var red = "4px solid red"
-    var blue = "4px solid rgba(000, 113, 206, 0.2)"
-    if(name === 'tags') {
-      var el = document.getElementById('template__tags').childNodes[0].childNodes[0];
-      el.style.border = !$scope.tags ? red : blue;
-    } else {
-      el.style.border = !$scope.template[name] ? red : blue;
-    }
-  };
+  // $scope.checkInput = function(name) {
+  //   var el = document.getElementById('template__' + name);
+  //   var red = "4px solid red"
+  //   var blue = "4px solid rgba(000, 113, 206, 0.2)"
+  //   if(name === 'tags') {
+  //     var el = document.getElementById('template__tags').childNodes[0].childNodes[0];
+  //     el.style.border = !$scope.tags ? red : blue;
+  //   } else {
+  //     el.style.border = !$scope.template[name] ? red : blue;
+  //   }
+  // };
 
   // Submits template in correct format
   $scope.submitTemplate = function() {
-    $scope.template.event = $scope.tags.reduce(function(eventList, currEvent) {
-      return eventList+= currEvent.abbreviation;
+    console.log($scope.selectedEvents);
+    $scope.template.event = $scope.selectedEvents.reduce(function(eventList, currEvent) {
+      return eventList += currEvent.eventName || currEvent.eventKey;
     }, '');
-    //temporarily sends first role: autocomplete/$scope.template.role can handle multiple
+    //temporarily sends only first role even though autocomplete can handle multiple
     $scope.template.role = $scope.template.role[0].name;
+    $scope.template.title = $scope.selectedProcedure[0].title;
+    $scope.template.description = $scope.selectedProcedure[0].text;
+    if ($scope.updatingProcedure) {
+      // make new obj without originalTitle and originalText k/v pairs
+      var updatedProcedure = {
+        $$hashKey: $scope.selectedProcedure[0].$$hashKey,
+        DateAdded: $scope.selectedProcedure[0].DateAdded,
+        id: $scope.selectedProcedure[0].id,
+        text: $scope.selectedProcedure[0].text,
+        title: $scope.selectedProcedure[0].title
+      };
+      Procedures.updateProcedure(updatedProcedure).then(function(){console.log('success!');}, function(){console.log('fail')});
+    }
     Template.submitTemplate($scope.template).then(submitSuccess, submitError);
-    console.log($scope.tags.selected);
   };
 
   var submitSuccess = function(response) {
