c5215854b0c26be3a809bf87faa7269f5908751f
Matthew Brooks <mdeanbrooks@gmail.com>
2016-01-04 20:33:42 -0800

    (fix) wip fixing auth logout bug

--- client/app/directives/navBar.js
+++ client/app/directives/navBar.js
@@ -5,9 +5,12 @@ angular.module('navBar', [])
       replace: true,
       scope: true,
       templateUrl: 'app/directives/nav-bar.html',
-      controller: function ($scope, $location) {
+      controller: function ($scope, $location, Auth) {
         $scope.getClass = function (currentPath) {
           return currentPath === $location.path().split('/')[1] ? 'active-nav' : '';
+        };
+        $scope.logout = function () {
+          Auth.logout();
         };
       }
     };
